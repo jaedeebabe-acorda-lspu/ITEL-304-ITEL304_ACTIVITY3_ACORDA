430564aef605f0ccd047b6af985adc726ee5bcd5
jaedeebabe-acorda-lspu <101610507+jaedeebabe-acorda-lspu@users.noreply.github.com>
2022-10-10 22:24:59 +0800

    Create ITEC304_ACTIVITY3_ACORDA.py

---
+++ ITEC304_ACTIVITY3_ACORDA.py
@@ -0,0 +1,36 @@
+from lib2to3.pgen2.token import GREATER
+from flask import Flask, request
+
+from datetime import datetime
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello():
+    return """<html><body>
+                    <h1>HELLO</h1>
+                    <form action='/greet'>
+                        What's' your Name? <input type='text' name='name'><br>
+                        Your Age? <input type='text' name='age'><br>
+                        <input type='submit' value='Submit'>
+              </body></html>
+           """
+
+@app.route('/greet')
+def greet():
+    name = request.args.get('name')
+    age = request.args['age']
+    if age == '':
+        msg = ''
+    else:
+        msg = age
+
+    return """
+        <html><body>
+                    <h2>{0} is {1}<h2>
+              </body></html>
+           """.format(name, msg)
+
+
+# Launch the Flask dev server
+app.run(host="localhost", debug=True)
